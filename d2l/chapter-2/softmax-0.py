6d6eafb93ae61cb28e0d9091c3176a40aa86f99c
Yellow-GGG <yellow_ggg@icloud.com>
2023-03-28 12:28:50 +0800

    Completed softmax

--- d2l/chapter-2/softmax-0.py
+++ d2l/chapter-2/softmax-0.py
@@ -1,15 +1,14 @@
 import torch
 from IPython import display
-import FM_load
 from d2l import torch as d2l
 
 batch_size = 256
-train_iter, test_iter = FM_load.load_data_fashion_mnist(batch_size)
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
 
-num_inputs = 748
+num_inputs = 784
 num_outputs = 10
 w = torch.normal(0, 0.01, size=(num_inputs, num_outputs), requires_grad=True)
-b = torch.normal(num_outputs, requires_grad=True)
+b = torch.zeros(num_outputs, requires_grad=True)
 
 def softmax(X):
     X_exp = torch.exp(X)
@@ -17,10 +16,11 @@ def softmax(X):
     return X_exp / partition # The broadcast mechanism is applied here
 
 def net(X):
-    return softmax(torch.matmul(X.reshape((-1, W.shape[0]), w) + b))
+    return softmax(torch.matmul(X.reshape(-1, w.shape[0]), w) + b)
 
 def cross_entropy(y_hat, y):
     return -torch.log(y_hat[range(len(y_hat)), y])
+    # 这里用了一些特殊的索引规则
 
 # 模型精度的计算与评估
 
@@ -39,3 +39,111 @@ def evaluate_accuracy(net, data_iter):
     for X, y in data_iter:
         metric.add(accuracy(net(X), y), y.numel())
     return metric[0] / metric[1]
+
+class Accumulator:
+    """For accumulating sums over 'n' variables."""
+    def __init__(self, n):
+        self.data = [0.0] * n
+    
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+def train_epoch_ch3(net, train_iter, loss, updater):
+    """Train a model within one epoch (defined in Chapter 3)."""
+    if isinstance(net, torch.nn.Module):
+        net.train()
+
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            updater.zero_grad()
+            l.mean().backword()
+            updater.step()
+        else:
+            l.mean().backward()
+            updater(X.shape[0])
+        metric.add(float(l.sum()),accuracy(y_hat, y), y.numel())
+
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+class Animator:
+    """For plotting data in animation."""
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
+                 figsize=(3.5, 2.5)):
+        # Incrementally plot multiple lines
+        if legend is None:
+            legend = []
+        d2l.use_svg_display()
+        self.fig, self.axes = d2l.plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes,]
+        # Use a lambda function to capture arguments
+        self.config_axes = lambda: d2l.set_axes(self.axes[0], xlabel, ylabel,
+                                                xlim, ylim, xscale, yscale,
+                                                legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def add(self, x, y):
+        # Add multiple data points into the figure
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        display.display(self.fig)
+        display.clear_output(wait=True)
+
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):
+    """Train the module"""
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9],
+                        legend=['train loss', 'train acc', 'test acc'])
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+    train_loss, train_acc = train_metrics
+    assert train_loss < 0.5, train_loss
+    assert train_acc <= 1 and train_acc > 0.7, train_acc
+    assert test_acc <= 1 and test_acc > 0.7, test_acc
+
+lr = 0.1
+
+def updater(batch_size):
+    return d2l.sgd([w, b], lr, batch_size)
+
+num_epochs = 10
+
+def predict_ch3(net, test_iter, n=6):
+    """Predict labels (defined in Chapter 3)."""
+    for X, y in test_iter:
+        break
+    trues = d2l.get_fashion_mnist_labels(y)
+    preds = d2l.get_fashion_mnist_labels(net(X).argmax(axis=1))
+    titles = [true + '\n' + pred for true, pred in zip(trues, preds)]
+    d2l.show_images(X[0:n].reshape(n, 28, 28), 1, n, titles=titles[0:n])
+
+if __name__ == "__main__":
+    train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, updater)
+    predict_ch3(net, test_iter)
